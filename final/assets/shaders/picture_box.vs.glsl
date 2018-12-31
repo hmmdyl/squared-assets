3cc76ca1b1249d3258d83021fcab162f02bd24f9
Dylan Graham <dylan.graham2000@outlook.com>
2019-01-01 02:27:54 +1100

    ?

--- final/assets/shaders/picture_box.vs.glsl
+++ final/assets/shaders/picture_box.vs.glsl
@@ -9,7 +9,7 @@ uniform vec2 Size;
 uniform mat4 Projection;
 
 void main() {
-    fTexCoord = vec2(Vertex.x, 1f - Vertex.y);
+    fTexCoord = vec2(Vertex.x, 1.0 - Vertex.y);
     vec2 vert = Position + (Vertex * Size);
     gl_Position = Projection * vec4(vert, 0, 1);
 }
