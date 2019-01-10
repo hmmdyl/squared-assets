68497173d08c128b75dc8f624b080a25f45b3f04
Dylan Graham <dylan.graham2000@outlook.com>
2019-01-10 13:25:15 +1100

    Vegetation shaders.. adding texturing support

--- final/assets/shaders/vegetation_voxel.vs.glsl
+++ final/assets/shaders/vegetation_voxel.vs.glsl
@@ -1,8 +1,13 @@
 #version 400 core
 
 layout(location = 0) in vec3 Vertex;
+layout(location = 1) in ivec4 Colour;
+layout(location = 2) in vec2 TexCoord;
 
 out vec3 fWorldPos;
+out vec2 fTexCoord;
+flat out vec3 fColour;
+flat out float fTexID;
 
 uniform mat4 ModelViewProjection;
 uniform mat4 Model;
@@ -10,4 +15,7 @@ uniform mat4 Model;
 void main() {
 	gl_Position = ModelViewProjection * vec4(Vertex, 1);
 	fWorldPos = (Model * vec4(Vertex, 1)).xyz;
+	fTexCoord = TexCoord;
+	fColour = Colour.xyz / vec3(255);
+	fTexID = Colour.w;
 }
