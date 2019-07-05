5d3ccabc6e1d80ad121a5582b50cec79341753a0
Dylan Graham <dylan.graham2000@outlook.com>
2019-07-05 12:50:25 +1000

    Texture tweaks

--- final/content/shaders/veggieProcessor.vs.glsl
+++ final/content/shaders/veggieProcessor.vs.glsl
@@ -9,12 +9,33 @@ out vec2 fTexCoord;
 flat out vec3 fColour;
 flat out float fTexID;
 
+uniform mat4 ModelView;
 uniform mat4 ModelViewProjection;
-uniform mat4 Model;
 
-void main() {
-	gl_Position = ModelViewProjection * vec4(Vertex, 1);
-	fWorldPos = (Model * vec4(Vertex, 1)).xyz;
+uniform float Time;
+const float Amplitude = 0.25;
+const float Wavelength = 1;
+const float Speed = 1;
+const vec2 Direction = vec2(1, 0);
+
+float wave(vec2 coord)
+{
+	float f = 2 * 3.14159 / Wavelength;
+	float phase = Speed * f;
+	float theta = dot(Direction, coord);
+	return Amplitude * (sin(theta * f + Time * phase) / 2 + 0.5);
+}
+
+void main() 
+{
+	//vec3 vertexModel = (Model * vec4(Vertex, 1)).xyz;
+	//float offset = wave(vertexModel.xz);
+	vec3 vertex = Vertex;
+	//vertex.x += offset * Direction.x;
+	//vertex.z += offset * Direction.y;
+
+	gl_Position = ModelViewProjection * vec4(vertex, 1);
+	fWorldPos = (ModelView * vec4(vertex, 1)).xyz;
 	fTexCoord = TexCoord;
 	fColour = Colour.xyz / vec3(255);
 	fTexID = Colour.w;// > 128 ? 1 : 0.5;
