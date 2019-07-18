f435b827289188a4e36bc411ea1bb03c529045d4
Dylan Graham <dylan.graham2000@outlook.com>
2019-07-18 18:35:52 +1000

    Added Time uniform to sky

--- final/content/shaders/sky.fs.glsl
+++ final/content/shaders/sky.fs.glsl
@@ -8,10 +8,11 @@ layout(location = 1) out vec3 WorldPositionOut;
 layout(location = 2) out vec3 NormalOut;
 
 uniform sampler2D ColourMap;
+uniform float Time;
 
 void main()
 {
-	DiffuseOut = texture(ColourMap, vec2(0.5, fHeight)).rgb;
+	DiffuseOut = texture(ColourMap, vec2(Time, fHeight)).rgb;
 	NormalOut = vec3(0, 0, 0);
 	WorldPositionOut = fWorldPos;
 }
