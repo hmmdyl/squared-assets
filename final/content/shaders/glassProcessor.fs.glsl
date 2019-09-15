3c43ba5bf8739260039f6f8fc8d5a14aa5cd3ca3
Dylan Graham <dylan.graham2000@outlook.com>
2019-09-15 13:46:44 +1000

    Glass test

--- final/content/shaders/glassProcessor.fs.glsl
+++ final/content/shaders/glassProcessor.fs.glsl
@@ -16,7 +16,7 @@ void main()
 	vec2 ndc = fClipSpace.xy / fClipSpace.w;
 	vec2 sceneTexCoord = clamp(ndc / 2.0 + 0.5, 0.0002, 0.9998);
 
-	DiffuseOut = mix(texture(SceneDiffuse, sceneTexCoord).rgb, fColour.xyz, 0.1);
+	DiffuseOut = mix(texture(SceneDiffuse, sceneTexCoord).rgb, fColour.xyz, 0.5);
 	NormalOut = fNormal;
 	WorldPositionOut = fWorldPos;
 }
